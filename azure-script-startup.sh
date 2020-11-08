07e295bcbe9c3c81ddc704119409e48ba46a591f
hemanth-gunda <hemanth.vgsom@gmail.com>
2020-11-08 15:15:57 +0530

    Create azure-script-startup.sh

---
+++ azure-script-startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo /usr/bin/anaconda/envs/py35/bin/conda install -c conda-forge xgboost==0.90 -n py35 --yes
+sudo /usr/bin/anaconda/envs/py35/bin/conda install -c conda-forge pyarrow==0.13.0 -n py35 --yes
