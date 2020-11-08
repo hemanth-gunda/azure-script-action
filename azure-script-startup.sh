af8a199a4b03914ea89b0e92a7da1eaa90a30195
hemanth-gunda <hemanth.vgsom@gmail.com>
2020-11-08 16:35:06 +0530

    Update azure-script-startup.sh

--- azure-script-startup.sh
+++ azure-script-startup.sh
@@ -2,3 +2,5 @@
 sudo /usr/bin/anaconda/envs/py35/bin/pip install --upgrade pip
 sudo /usr/bin/anaconda/envs/py35/bin/pip install xgboost==0.90
 sudo /usr/bin/anaconda/envs/py35/bin/pip install pyarrow==0.13.0
+sudo rm -rf /usr/bin/anaconda/envs/py35/lib/libgomp.so.1
+sudo ln /usr/lib/x68_64-linux-gnu/libgomp.so.1.0.0 /usr/bin/anaconda/envs/py35/lib/libgomp.so.1
